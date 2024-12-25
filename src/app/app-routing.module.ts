757aff945c373f196265cc882849cd7a52431b72
Sandy3pawar <107829646+Sandy3pawar@users.noreply.github.com>
2024-12-25 09:16:38 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,18 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './home/home.component';
+// const routes: Routes = [
+//   { path: 'register', component: RegisterComponent },
+//   {path: 'login', component: LoginComponent},
+//    {path: 'home', component: HomeComponent},
+// {path: '**', redirectTo: 'home'}];
+
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
-  {path: 'login', component: LoginComponent},
-   {path: 'home', component: HomeComponent},
-{path: '**', redirectTo: 'home'}];
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: 'home' } // This will redirect to home if no route matches
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
