ab9d46d8b81227c55d7681572e0f513a6f747ee5
Sandy3pawar <107829646+Sandy3pawar@users.noreply.github.com>
2024-12-25 09:59:45 +0530

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000/api/auth', // Replace with your backend URL
+       // apiUrl: 'http://localhost:5000/api/auth', // Replace with your backend URL
+    apiUrl:'https://task-management-backend-90ky.onrender.com/api/auth'
 };
